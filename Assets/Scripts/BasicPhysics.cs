0cc0afbab3353ed80ef7c4035cc2c18bf223b9ce
Juno Morrow <sabotaione@gmail.com>
2018-10-17 16:43:45 -0400

    added w5 material

---
+++ Assets/Scripts/BasicPhysics.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicPhysics : MonoBehaviour {
+
+	Rigidbody myBody;
+	public float forceAmt;
+	// Use this for initialization
+	void Start () {
+		myBody = GetComponent<Rigidbody>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.W)){
+			myBody.AddForce(Vector3.forward * forceAmt);
+		}
+		if (Input.GetKey(KeyCode.S)){
+			myBody.AddForce(Vector3.back * forceAmt);
+		}
+		if (Input.GetKey(KeyCode.A)){
+			myBody.AddForce(Vector3.left * forceAmt);
+		}
+		if (Input.GetKey(KeyCode.D)){
+			myBody.AddForce(Vector3.right * forceAmt);
+		}
+	}
+}
---
+++ Assets/Scripts/Seek.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seek : MonoBehaviour {
+
+	Rigidbody myBody;
+	public Transform target;
+	public float forceAmt;
+
+	// Use this for initialization
+	void Start () {
+		myBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 direction = Vector3.Normalize(target.position - transform.position);
+
+		myBody.AddForce(direction * forceAmt);
+		
+	}
+}
